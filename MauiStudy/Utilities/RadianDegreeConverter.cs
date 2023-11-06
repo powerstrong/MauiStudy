821b3620126f15daf56a2e69bc254233b0e60cd3
jslee <jslee@plaif.com>
2023-11-06 22:23:57 +0900

    finish tcp test

---
+++ MauiStudy/Utilities/RadianDegreeConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiStudy.Utilities
+{
+    class RadianDegreeConverter
+    {
+        public double ConvertToDegree(int integralPart, int fractionalPart)
+        {
+            var us1 = (ushort)integralPart;
+            var us2 = (ushort)fractionalPart;
+
+            uint binary32Bit = (uint)((us1 << 16) | us2);
+            float degree = BitConverter.ToSingle(BitConverter.GetBytes(binary32Bit), 0);
+            return (double)degree;
+        }
+
+        public double ConvertToRadian(int integralPart, int fractionalPart)
+        {
+            var us1 = (ushort)integralPart;
+            var us2 = (ushort)fractionalPart;
+
+            double radian = ConvertToDegree(us1, us2) * Math.PI / 180;
+            return radian;
+        }
+    }
+}
--- MauiStudy/ViewModel/ModbusTcpTestViewModel.cs
+++ MauiStudy/ViewModel/ModbusTcpTestViewModel.cs
@@ -1,10 +1,5 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MauiStudy.ViewModel
 {
@@ -16,11 +11,45 @@ namespace MauiStudy.ViewModel
         [ObservableProperty]
         int _port = 502;
 
+        [ObservableProperty]
+        string _jointAngle;
+
+        [ObservableProperty]
+        bool _connectButtonEnable = true;
+
+        [ObservableProperty]
+        bool _updateButtonEnable = false;
+
+        private EasyModbus.ModbusClient _client;
+
         [RelayCommand]
         void Connect()
         {
-            EasyModbus.ModbusClient client = new(Ip, Port);
-            client.Connect();
+            _client = new(Ip, Port);
+            _client.Connect();
+
+            ConnectButtonEnable = false;
+            UpdateButtonEnable = true;
+        }
+
+        [RelayCommand]
+        void UpdateJointAngle()
+        {
+            ushort address = 201;
+            ushort size = 12;
+            var result = _client.ReadInputRegisters(address, size);
+            var angles = new double[6];
+
+            // double 6개로 변환
+            Utilities.RadianDegreeConverter conv = new();
+            for (int i = 0; i < 6; i++)
+            {
+                double rad = conv.ConvertToDegree(result[i * 2 + 1], result[i * 2]);
+                angles[i] = rad;
+            }
+
+            JointAngle = string.Format("{0}, {1}, {2}, {3}, {4}, {5}",
+                angles[0], angles[1], angles[2], angles[3], angles[4], angles[5]);
         }
     }
 }
